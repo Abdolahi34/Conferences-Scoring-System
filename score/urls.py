44aa1a662a357eb90f71fcd7f017efbd4768a8c7
Abdolahi <94745812+Abdolahi34@users.noreply.github.com>
2023-11-02 23:46:06 +0330

    add lessons list and users list pages

--- score/urls.py
+++ score/urls.py
@@ -5,4 +5,6 @@ from score import views
 app_name = 'score'
 urlpatterns = [
     path('lessons/', views.lessons_list, name='score_lessons'),
+    path('lessons/', views.lessons_list, name='lessons'),
+    path('lessons/<int:lesson_id>/', views.users_list, name='users'),
 ]
--- score/views.py
+++ score/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import Group
+from django.contrib.auth.models import Group, User
+from django.views import View
+from django.utils.decorators import method_decorator
+from django.db.models import Q
+from django.core.exceptions import PermissionDenied
+
+from score import models
 
 
 @login_required
@@ -8,4 +14,16 @@ def lessons_list(request):
     lessons = Group.objects.all()
     args = {'lessons': lessons}
     return render(request, 'score/lessons_list.html', args)
+
+
+@login_required
+def users_list(request, lesson_id):
+    # بررسی عضویت دانشجو در درس
+    if User.objects.filter(Q(groups__id=lesson_id) & Q(id=request.user.id)).exists():
+        lesson = Group.objects.get(id=lesson_id)
+        users = User.objects.filter(groups__id=lesson_id)
+        args = {'lesson': lesson, 'users': users}
+        return render(request, 'score/users_list.html', args)
+    else:
+        raise PermissionDenied('a')
 
