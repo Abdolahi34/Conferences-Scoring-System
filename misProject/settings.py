f59776f5a173171e0230abd34b739db2e9c6d36a
Abdolahi <94745812+Abdolahi34@users.noreply.github.com>
2023-11-16 10:50:32 +0330

    Remove extras
    Remove additional files and codes

--- misProject/settings.py
+++ misProject/settings.py
@@ -59,7 +59,6 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [os.path.join(BASE_DIR, 'templates')]
-        # 'DIRS': [BASE_DIR / 'templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
