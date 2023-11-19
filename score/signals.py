4981e1873bf18c002a4442328d2d1cd014fdaec8
Abdolahi <94745812+Abdolahi34@users.noreply.github.com>
2023-11-19 22:49:11 +0330

    Automatic creation of lesson
    Automatic creation of lesson instance after creating the group

--- score/signals.py
+++ score/signals.py
@@ -1,6 +1,6 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.contrib.auth.models import User
+from django.contrib.auth.models import Group, User
 
 from score import models
 
@@ -11,3 +11,11 @@ def run_after_save_user_model(sender, instance, created, **kwargs):
     if not models.Preferential.objects.filter(user=instance).exists():
         preferential = models.Preferential(user=instance)
         preferential.save()
+
+
+# Automatic creation of Lesson instance after creation of each group
+@receiver(post_save, sender=Group)
+def run_after_save_group_model(sender, instance, created, **kwargs):
+    if created:
+        lesson = models.Lesson(group=instance)
+        lesson.save()
