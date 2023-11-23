2cb67e3df5371faff52dce00488fef6cbe22192d
Abdolahi <94745812+Abdolahi34@users.noreply.github.com>
2023-11-23 07:08:40 +0330

    Transfer the content of User signal to admin.py

--- misProject/admin.py
+++ misProject/admin.py
@@ -3,6 +3,8 @@ from django.contrib.auth import get_user_model
 from django.utils.translation import gettext_lazy as _
 from django.contrib import admin
 
+from score import models
+
 admin.site.unregister(get_user_model())
 
 
@@ -17,3 +19,18 @@ class CustomUserAdmin(UserAdmin):
         }),
     )
     list_display = ('username', 'email', 'first_name', 'last_name', 'last_login', 'date_joined', 'is_staff')
+
+    def save_model(self, request, obj, form, change):
+        # Automatic creation of Preferential instance after creation of each user
+        # if on update
+        if 'groups' in form.cleaned_data:
+            user_groups = form.cleaned_data['groups']
+            for user_group in user_groups:
+                lesson = models.Lesson.objects.filter(group_id=user_group.id)
+                if lesson:
+                    lesson = lesson[0]
+                else:
+                    user_group.save()
+                    lesson = user_group.lesson_group
+                models.set_preferential(lesson, lesson.initial_score)
+        super(CustomUserAdmin, self).save_model(request, obj, form, change)
--- score/signals.py
+++ score/signals.py
@@ -1,25 +1,8 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.contrib.auth.models import Group, User
+from django.contrib.auth.models import Group
 
 from score import models
-
-
-# Automatic creation of Preferential instance after creation of each user
-@receiver(post_save, sender=User)
-def run_after_save_user_model(sender, instance, created, **kwargs):
-    user_groups = instance.groups.all()
-    for user_group in user_groups:
-        lesson = models.Lesson.objects.filter(group_id=user_group.id)
-        if lesson:
-            lesson = lesson[0]
-            initial_score_list = models.get_lesson_initial_score_list(lesson)
-            models.set_preferential(lesson, initial_score_list)
-        else:
-            user_group.save()
-            lesson = user_group.lesson_group.all()[0]
-            initial_score_list = models.get_lesson_initial_score_list(user_group)
-            models.set_preferential(lesson, initial_score_list)
 
 
 # Automatic creation of Lesson instance after creation of each group
