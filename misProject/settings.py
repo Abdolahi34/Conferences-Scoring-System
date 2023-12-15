460c0f8a676a256ca1a1f338778d4dff2500593d
Abdolahi <94745812+Abdolahi34@users.noreply.github.com>
2023-12-15 11:18:44 +0330

    Add import-export module

--- misProject/settings.py
+++ misProject/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_jsonform',
+    'import_export',
     'misProject',
     'score.apps.ScoreConfig',
 ]
